39298b63da81dfc941c5e518836dc0a6ae4daef5
Josh Caron <joshua@minshane.com>
2025-08-03 15:25:51 -0400

    Added in template

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
-print("hello World")
-print("test")
+from helper import load_stock_data
+from sma import compute_sma
+from linear_regression import train_and_predict
+
+def main():
+    symbol = "AAPL"
+    df = load_stock_data(symbol)
+    compute_sma(df)
+    train_and_predict(df)
+
+if __name__ == "__main__":
+    main()
