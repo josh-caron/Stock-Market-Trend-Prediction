b3ef18860e64f83d8d6f7d550eb0fe67f6961404
Josh Caron <joshua@minshane.com>
2025-08-03 15:25:41 -0400

    Added in helper function for main

---
+++ src/helper.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+import pandas as pd
+
+def load_stock_data(symbol, start="2022-01-01", end="2023-01-01"):
+    df = yf.download(symbol, start=start, end=end)
+    df = df[["Close"]]
+    df.dropna(inplace=True)
+    df.reset_index(inplace=True)
+    return df
