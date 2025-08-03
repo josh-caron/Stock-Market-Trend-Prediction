38bd5e6aa3fde05a97bafa6e9063cd23de504745
Josh Caron <joshua@minshane.com>
2025-08-03 16:06:01 -0400

    Silenced FutureWarning

--- src/helper.py
+++ src/helper.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 
 def load_stock_data(symbol, start="2022-01-01", end="2023-01-01"):
-    df = yf.download(symbol, start=start, end=end)
+    df = yf.download(symbol, start=start, end=end, auto_adjust=True)
     df = df[["Close"]]
     df.dropna(inplace=True)
     df.reset_index(inplace=True)
